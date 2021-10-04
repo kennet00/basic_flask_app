f4a50ed3d00d4b85d28ab7a6f40e4815c6089d02
kenneth <nkenneth012@gmail.com>
2021-10-04 11:40:45 -0500

    add config

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
+app.config.from_object(env_config)
 
 
 @app.route('/')
 def yo():
-    return '<p>added staging pipeline</p>'
+    secret_key = app.config.get('SECRET_KEY')
+    return f'the secret key is not {secret_key} haha'
